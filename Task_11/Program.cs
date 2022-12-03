f83ae3b8f7dbcff16c361022ca930a987ba922b4
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-03 14:38:01 +0300

    Task_11

---
+++ Task_11/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Сгерируемое число: {number}");
+int n3 = number % 10; 
+int n1 = number/100;
+int result = n1 * 10 + n3;
+
+Console.WriteLine(result);
+
