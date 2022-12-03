86550d0f96b565760c73acd970592b2c922cfb00
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-03 15:16:46 +0300

    Task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 1-ое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if(n1 * n1 == n2 || n2 * n2 == n1)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
