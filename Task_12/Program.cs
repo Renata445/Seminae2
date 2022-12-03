16a1b0ca721a3170132bfc1c4bd6f51dae099ccf
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-03 14:52:12 +0300

    Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number2 % number == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, {number2 % number}");
+}
